874442a8808dc57eef4c2aa8738a71c8526444aa
Jabez Villan C. Pastores <152822763+JabezPastores@users.noreply.github.com>
2025-02-10 06:19:41 +0000

    Activity 3

---
+++ MyScript.sh
@@ -0,0 +1,5 @@
+echo "What is your name? "
+read name
+echo "What is your favorite food? "
+read food
+echo "Hi $name, I see you like $food"
---
+++ myscript.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+echo "enter your favorite color: "
+read color
